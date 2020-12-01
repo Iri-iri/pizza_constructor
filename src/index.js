84523e11dc17c931b5faed4c9a3e59e7b62fa678
Ira Levkovich <6346390@mail.ru>
2020-12-02 02:18:17 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Ingredients from './components/Ingredients';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Ingredients />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
